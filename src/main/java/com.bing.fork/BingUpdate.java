ef7bf507edf24aef1876d0249337dc0e42d1eaa8
yunyun.zhao@okcoin.com <yunyun.zhao@okcoin.com>
2020-04-11 21:09:18 +0800

    bing update something

---
+++ src/main/java/com.bing.fork/BingUpdate.java
@@ -0,0 +1,14 @@
+package com.bing.fork;
+
+/**
+ * @Authon zhaoyunyun
+ * @Date 2020/4/11 9:07 PM
+ **/
+
+public class BingUpdate {
+    public static void main(String[] args) {
+        System.out.println("bing update");
+        System.out.println("bing commit");
+        System.out.println("bing push");
+    }
+}
