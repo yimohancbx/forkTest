30953cf686d4afd264aa9dcd7f62dfbb48185609
yunyun.zhao@okcoin.com <yunyun.zhao@okcoin.com>
2020-04-11 20:17:19 +0800

    update gittest

---
+++ src/main/java/com/yun/fork/GitTest.java
@@ -0,0 +1,12 @@
+package com.yun.fork;
+
+/**
+ * @Authon zhaoyunyun
+ * @Date 2020/4/11 8:16 PM
+ **/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("2");
+    }
+}
