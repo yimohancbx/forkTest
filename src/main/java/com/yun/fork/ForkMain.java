e2622a5749a5aec710ce523fefca7d0b35bead33
yunyun.zhao@okcoin.com <yunyun.zhao@okcoin.com>
2020-04-11 20:06:02 +0800

    push

---
+++ src/main/java/com/yun/fork/ForkMain.java
@@ -0,0 +1,12 @@
+package com.yun.fork;
+
+/**
+ * @Authon zhaoyunyun
+ * @Date 2020/4/11 8:04 PM
+ **/
+
+public class ForkMain {
+    public static void main(String[] args) {
+        System.out.println("s");
+    }
+}
